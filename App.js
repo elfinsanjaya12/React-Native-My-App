0e68c7b220442944cac51f4f527e2eecb3037852
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2018-12-23 05:47:38 -0800

    Export And Import

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Contoh from './contoh'
+import {hallo, hello} from './contoh'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Hello Elfin Sanjaya</Text>
+        <Text>{hallo}</Text>
+        <Text>{hello}</Text>
       </View>
     );
   }
---
+++ contoh.js
@@ -0,0 +1,7 @@
+export const hello = 'Hello World'
+export const hallo = 'Hallo World'
+
+export default {
+    hello,
+    hallo
+}
